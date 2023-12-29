bc014dd87378622e5f21705991045c132e0d8cea
Zabihullah <Zabihullah18381@gmail.com>
2023-12-28 17:22:51 -0800

    firs

---
+++ templete.py
@@ -0,0 +1,15 @@
+import os
+from pathlib import Path
+
+
+list_of_files  = [
+
+    ".github/workflows",
+    "src/__init__.py",
+    "src/components/__init__.py",
+    "src/components/data_ingestion.py",
+    "src/components/data_transformation.py",
+    "src/components/model_trainer.py",
+    "src/components/model_evaluation.py"
+
+]
