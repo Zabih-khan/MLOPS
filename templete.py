7e3a126d97d2e97c477598744a2021ceb0d05d1e
Zabihullah <Zabihullah18381@gmail.com>
2023-12-30 12:52:12 -0800

    first commit

--- templete.py
+++ templete.py
@@ -10,6 +10,14 @@ list_of_files  = [
     "src/components/data_ingestion.py",
     "src/components/data_transformation.py",
     "src/components/model_trainer.py",
-    "src/components/model_evaluation.py"
+    "src/components/model_evaluation.py",
+    "src/pipeline/__init__.py",
+    "src/pipeline/training_pipeline.py",
+    "src/pipeline/prediction_pipeline.py",
+    "src/utils/utils.py",
+    "test/unit/__init__.py",
+    "test/integresion/__init__.py",
+
+       
 
 ]
