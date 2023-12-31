f442181a955e890abacc284ada3ed96c56e845ae
Zabihullah <Zabihullah18381@gmail.com>
2023-12-31 15:11:31 -0800

    templetes

--- templete.py
+++ templete.py
@@ -1,10 +1,9 @@
 import os
 from pathlib import Path
+import logging
 
-
-list_of_files  = [
-
-    ".github/workflows",
+list_of_files = [
+    ".github/workflows/.gitkeep",
     "src/__init__.py",
     "src/components/__init__.py",
     "src/components/data_ingestion.py",
@@ -15,9 +14,28 @@ list_of_files  = [
     "src/pipeline/training_pipeline.py",
     "src/pipeline/prediction_pipeline.py",
     "src/utils/utils.py",
+    "src/logger/logging.py",
+    "src/exception/exception",  
+    "src/utils/__init__.py",
     "test/unit/__init__.py",
-    "test/integresion/__init__.py",
-
-       
-
+    "test/integration/__init__.py",  
+    "init_setup.sh",
+    "requirements.txt",
+    "requirement_dev.txt",  
+    "setup.py",
+    "setup.config",
+    "pyproject.toml",
+    "tox.ini",
+    "experiments.ipynb"
 ]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating Directory: {filedir} for file: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, 'w'):
+            pass
